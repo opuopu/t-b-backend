219e29b926491cdc7c9d96e74b58a74180f66b76
opuopu <nurmdopu428@gmail.com>
2024-01-13 12:19:08 +0600

    refactor: refactor apis

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,7 @@ import authRoutes from "./auth.route.js";
 import { Router } from "express";
 import otpRoutes from "./otp.routes.js";
 import homeRoutes from "./home.route.js";
-import homeCategoryRoutes from "./homeCategory.Route.js";
 import roomRoutes from "./room.route.js";
-import roomPresetRoutes from "./roomPreset.route.js";
 const router = Router();
 const moduleRoutes = [
   {
@@ -19,14 +17,7 @@ const moduleRoutes = [
     path: "/homes",
     route: homeRoutes,
   },
-  {
-    path: "/home-categories",
-    route: homeCategoryRoutes,
-  },
-  {
-    path: "/room-presets",
-    route: roomPresetRoutes,
-  },
+
   {
     path: "/rooms",
     route: roomRoutes,
