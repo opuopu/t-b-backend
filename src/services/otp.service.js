fe5d1d03face36ae950da9c9ed11e1cb7500729d
opuopu <nurmdopu428@gmail.com>
2024-05-08 10:14:48 +0600

    fix: fix issues

--- src/services/otp.service.js
+++ src/services/otp.service.js
@@ -14,11 +14,8 @@ import { nextFiveDay } from "../utils/date.utils.js";
 
 const createAnOtpIntoDB = async ({ email, type }) => {
   const user = await User.findOne({ email: email });
-  if (user) {
-    throw new AppError(
-      httpStatus.NOT_FOUND,
-      "User already exist with this email"
-    );
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, "User not found");
   }
   const otp = generateOtp();
   const expiresAt = new Date(Date.now() + 3600000);
